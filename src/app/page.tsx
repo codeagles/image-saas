1db670e4f0a8c9957173d309f4b255431ad226de
codeagles <hcn0521@outlook.com>
2024-10-17 22:35:39 +0800

    集成auth，并且获取session

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,21 +2,23 @@ import { Button } from "@/components/Button";
 import { Input } from "@/components/Input";
 import { Textarea } from "@/components/Textarea";
 import {db} from "@/server/db/db";
+import {signIn} from "@/auth";
+import UserAvatar from "@/app/userinfo";
 
 
 export default async function Home() {
-    const users = await db.query.Users.findMany();
     return <div className="h-screen flex justify-center items-center">
-      <form className="w-full max-w-md flex flex-col gap-4">
-          <h1 className="text-center text-2xl font-bold">Create App</h1>
-        <Input name="name" placeholder="App Name"></Input>
-        <Textarea name="description" placeholder="Description"></Textarea>
-        <Button type="submit">Submit</Button>
-      </form>
+        <form className="w-full max-w-md flex flex-col gap-4">
+            <h1 className="text-center text-2xl font-bold">Create App</h1>
+            <Input name="name" placeholder="App Name"></Input>
+            <Textarea name="description" placeholder="Description"></Textarea>
+            <Button type="submit">Submit</Button>
+        </form>
 
-        {users.map((user)=>(
-            <div key={user.id}>{user.name}</div>
-        ))}
+        <div>
+            <UserAvatar/>
+        </div>
     </div>
-  ;
+
+        ;
 }
--- src/auth.ts
+++ src/auth.ts
@@ -1,38 +1,12 @@
 import NextAuth from "next-auth"
-import Credentials from "next-auth/providers/credentials"
+import GitLab from "next-auth/providers/gitlab"
+import {DrizzleAdapter} from "@auth/drizzle-adapter";
+import {db} from "@/server/db/db";
+
 
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-    providers: [
-        Credentials({
-            // You can specify which fields should be submitted, by adding keys to the `credentials` object.
-            // e.g. domain, username, password, 2FA token, etc.
-            credentials: {
-                username: {
-                    lable: "Username",
-                    type: "text",
-                    placeholder: "jsmith",
-                },
-                password : {
-                    label: "Password",
-                    type: "password"
-                },
-            },
-            authorize: async (credentials) => {
-                if (!credentials) {
-                    return null
-                }
-
-                const {username, password} = credentials
-                if (username !== "bob" && password !== "123456") {
-                    return null
-                }
-                // return user object with their profile data
-                return {
-                    id: "1",
-                    ...credentials,
-                }
-            },
-        }),
-    ],
+    adapter: DrizzleAdapter(db),
+    providers: [GitLab],
+    secret: process.env.AUTH_SECRET,
 })
